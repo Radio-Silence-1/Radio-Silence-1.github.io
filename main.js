732295b8bc6141080015490fb92399e387d450e7
Radio-Silence-1 <98431859+Radio-Silence-1@users.noreply.github.com>
2023-11-25 10:47:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-alert("hehehehe")
+dont={
+  name: "dont",
+  description: `dont`,
+  isFieldTech: false,
+  maxCount: 1,
+  count: 0,
+  frequency: 1,
+  frequencyDefault: 200,
+  allowed() {
+      return true
+  },
+  requires: "",
+  effect() {
+      setInterval(function(){
+      Matter.Body.setVelocity(player, {
+          x: player.velocity.x + 1 * Math.cos(m.angle),
+          y: player.velocity.y + 1 * Math.sin(m.angle)
+      });
+          },1)
+  },
+  remove() {
+      tech.customWIMP = 0
+  }
+}
+eval(tech.tech.push(dont)
