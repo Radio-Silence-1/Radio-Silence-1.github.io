8f36d3ffe44aaaaecc02df7885d7a9edb104aac0
Radio-Silence-1 <98431859+Radio-Silence-1@users.noreply.github.com>
2023-11-25 21:31:46 -0600

    Update browser.js

--- browser.js
+++ browser.js
@@ -1,6 +1,6 @@
 (async () => {     
   alert("To select a mod, on the next popup, type the identifier of the mod you would like to download.\nFor example: 'Example mod (1)', to get the mod, you input '1'");   
-  let mod=prompt("Example Mod (1)\nPlaceholder (2)");    
+  let mod=prompt("Eye Mod (1)\nPlaceholder (2)");    
   if(mod=="1"){        
     const scriptText = await (       
       await fetch('https://raw.githubusercontent.com/Radio-Silence-1/ngon-mod-browser/main/main.js')).text();         
