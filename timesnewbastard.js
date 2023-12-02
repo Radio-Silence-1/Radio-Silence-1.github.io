a6893eff33ca236494b83f069b277f45c08ddd20
Radio-Silence-1 <98431859+Radio-Silence-1@users.noreply.github.com>
2023-12-01 20:17:12 -0600

    Update timesnewbastard.js

--- timesnewbastard.js
+++ timesnewbastard.js
@@ -1,6 +1,3 @@
 let style = document.createElement("style"); 
-style.innerHTML = '@font-face {font-family: "Times New Bastard";
-src: url("https://swaous.asuscomm.com/TimesNewBastard-RegularWeb.ttf");
-font-feature-settings: "kern" on, "liga" on;
-text-rendering: optimizeLegibility;} * {font-family: "Times New Bastard" !important;}'; 
+style.innerHTML = '@font-face {font-family: "Times New Bastard";src: url("https://swaous.asuscomm.com/TimesNewBastard-RegularWeb.ttf");font-feature-settings: "kern" on, "liga" on;text-rendering: optimizeLegibility;} * {font-family: "Times New Bastard" !important;}'; 
 document.head.appendChild(style);
