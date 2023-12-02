64e7d2ec61e833b7dd1087dba7947459bd9a308b
Radio-Silence-1 <98431859+Radio-Silence-1@users.noreply.github.com>
2023-12-01 20:22:53 -0600

    Update timesnewbastard.js

--- timesnewbastard.js
+++ timesnewbastard.js
@@ -1,3 +1,4 @@
 let style = document.createElement("style"); 
+console.log("this is debug for me (Radio), idk if the mod broke in github, but not console, or if its not updating")
 style.innerHTML = '@font-face {font-family: "Times New Bastard";src: url("https://swaous.asuscomm.com/TimesNewBastard-RegularWeb.ttf");font-feature-settings: "kern" on, "liga" on;text-rendering: optimizeLegibility;} * {font-family: "Times New Bastard" !important;}'; 
 document.head.appendChild(style);
